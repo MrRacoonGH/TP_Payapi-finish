7b332e93d31289f705f4a4559a9c2dfde9ffc543
MrRacoonGH <hadrien181107@gmail.com>
2025-08-14 15:37:36 +0200

    js-menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log("hello")
+document.getElementById("burger-menu").addEventListener("click", function() {
+    document.getElementById("side-menu").classList.add("active");
+});
+
+document.getElementById("close-menu").addEventListener("click", function() {
+    document.getElementById("side-menu").classList.remove("active");
+});
